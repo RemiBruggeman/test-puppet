e26461a73aa74c16600c4b934c842caadf899b39
RemiBruggeman <remi@starcoders.com>
2015-06-12 11:59:31 +0200

    finalise ssh module and push to servers

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,15 +1,19 @@
+class base {
+	include sudo
+	include ssh
+}
 node 'gateway.test.local' {
-
+	include base
 }
 node 'puppet.test.local' {
-
+	include base
 }
 node 'mail.test.local' {
-
+	include base
 }
 node 'db.test.local' {
-
+	include base
 }
 node 'default' {
-
+	include base
 }
